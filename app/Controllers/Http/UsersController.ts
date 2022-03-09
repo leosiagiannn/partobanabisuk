9f15c4a52dbfaf76128b615f4d93fde419e122fc
Leonardo <leonardo@digits.id>
2022-03-09 22:28:11 +0700

    Todo List Simple Crud

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -30,4 +30,29 @@ export default class UsersController {
       return response.status(500).json({ code: 500, status: 'error', message: err.message })
     }
   }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const input = request.only(['username', 'email'])
+    try {
+      const users = await User.findBy('id', params.id)
+      users?.merge(input)
+
+      await users?.save()
+
+      return response.status(200).json({ code: 200, status: 'success', data: users })
+    } catch (err) {
+      return response.status(500).json({ code: 500, status: 'error', message: err.message })
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    try {
+      const users = await User.findBy('id', params.id)
+      await users?.delete()
+
+      return response.status(200).json({ code: 200, status: 'success', data: users })
+    } catch (err) {
+      return response.status(500).json({ code: 500, status: 'error', message: err.message })
+    }
+  }
 }
